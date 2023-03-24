66ff69f6e5df2f9121d333698f0f60a66d8a8a3d
highjune <highjune37@gmail.com>
2023-03-25 02:07:24 +0900

    실무에서의 데이터 접근 기술 활용방안(jpa + queryDsl)

---
+++ src/main/java/hello/itemservice/service/ItemServiceV2.java
@@ -0,0 +1,45 @@
+package hello.itemservice.service;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ItemServiceV2 implements ItemService { // 실무에서는 아래처럼 repository 의존.(jpa + queryDsl)
+
+    private final ItemRepositoryV2 itemRepositoryV2;    // jpa 바로 의존함 (간단한 쿼리들)
+    private final ItemQueryRepositoryV2 itemQueryRepositoryV2;  // queryDsl (동적 쿼리 등 복잡한 쿼리들)
+
+    @Override
+    public Item save(Item item) {
+        return itemRepositoryV2.save(item);
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = itemRepositoryV2.findById(itemId).orElseThrow();
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return itemRepositoryV2.findById(id);
+    }
+
+    @Override
+    public List<Item> findItems(ItemSearchCond cond) {
+        return itemQueryRepositoryV2.findAll(cond);
+    }
+}
